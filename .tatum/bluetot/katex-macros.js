d43546975c5602023fa5a8706c6d8b8053ce08ab
Nok Hang Lo <bluetotkc@gmail.com>
2026-09-07 20:50:23 +0100

    feat: add .tatum directory and re-render notes

---
+++ .tatum/bluetot/katex-macros.js
@@ -0,0 +1,9 @@
+window.katexMacros = {
+    "\\R": "\\mathbb{R}",
+    "\\C": "\\mathbb{C}",
+    "\\Q":"\\mathbb{Q}",
+    "\\Z": "\\mathbb{Z}",
+    "\\N": "\\mathbb{N}",
+    "\\vv": ["\\vec{#1}", 1],
+    "\\bb": ["\\textbf{#1}", 1]
+};
